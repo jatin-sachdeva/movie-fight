b854acd1cb0b8ab4ef9daf11e40b9ac3abb7dee3
JatinS <jatin@JatinS>
2021-02-08 21:18:39 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const fetchData = async function(searchValue) {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: 'be698af8',
+			s: searchValue
+		}
+	});
+	if (response.data.Error) return [];
+
+	return response.data.Search;
+};
+
+// the html code for the widget setup inside a divison named autocomplete
+const autocomplete = document.querySelector('.autocomplete');
+autocomplete.innerHTML = `
+	<label for="search"> Search a Movie </label>
+	
+	<div class="dropdown">
+	<input type="text" id="search"/>
+		<div class ="dropdown-menu">
+			<div class ="dropdown-content results">
+			</div>
+		</div>
+	</div>
+`;
+
+const search = document.querySelector('#search');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+async function onInput(event) {
+	const movies = await fetchData(event.target.value);
+	// checking if no results are present inside movies
+	if (!movies.length) return;
+	dropdown.classList.add('is-active');
+	// clearing the previous search results
+	resultsWrapper.innerHTML = null;
+	// iterating the movie to display title and image
+
+	for (let movie of movies) {
+		// creating element for every returned result and apprnding it to the divison
+		const resultsItems = document.createElement('a');
+		resultsItems.classList.add('dropdown-item');
+		const imgSrc = movie.Poster != 'N/A' ? movie.Poster : '';
+
+		resultsItems.innerHTML = `
+		<img src="${imgSrc}">
+		${movie.Title}
+		`;
+		// resultsItems.addEventListener('click', (event) => {
+		// 	search.value = event.target.innerText;
+		// 	dropdown.classList.remove('is-active');
+		// });
+		resultsItems.addEventListener('click', movieSelected);
+		resultsWrapper.appendChild(resultsItems);
+	}
+}
+
+async function movieSelected(event) {
+	search.value = event.target.innerText;
+	dropdown.classList.remove('is-active');
+	const result = await fetchMovie(search.value);
+}
+
+const fetchMovie = async (title) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: 'be698af8',
+			t: title
+		}
+	});
+	return response.data;
+};
+
+search.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', (event) => {
+	// .contains check whether the passed element(passed inside of .includes) is child of the targeted element(on which the method is applied)
+	autocomplete.contains(event.target) ? '' : dropdown.classList.remove('is-active');
+});
---
+++ utility.js
@@ -0,0 +1,11 @@
+function debounce(func,delay){
+    let timeoutId;
+    return (...args)=>{
+        if(timeoutId){
+        clearTimeout(timeoutId);
+        }
+        timeoutId=setTimeout(()=>{
+            func.apply(null,args)
+        },delay)
+    }
+}
